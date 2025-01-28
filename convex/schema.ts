62e38d29d6be9f4612729f8d442cf3af1cc29ca4
abhishek1165 <abhishekkachha12345@gmail.com>
2025-01-29 03:22:06 +0530

    we have implemented webhooks for user creation

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,13 +1,14 @@
-import {defineSchema, defineTable} from "convex/server"
-import { v } from "convex/values"
-
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export default defineSchema({
-    users:defineTable({
-        name:v.string(),
-        email:v.string(),
-        image:v.optional(v.string()),
-        role:v.union(v.literal("admin"),v.literal("user")),
-        clerkId:v.string(), 
-    }).index("by_clerkId",["clerkId"]),
-})
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    image: v.optional(v.string()),
+    role: v.union(v.literal("candidate"), v.literal("interviewer")),
+    clerkId: v.string(),
+  }).index("by_clerk_id", ["clerkId"]),
+  
+});
+//https://precise-leech-31.clerk.accounts.dev
