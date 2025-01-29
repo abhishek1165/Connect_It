bc7350f377016ea80ac8a5be5dfce4f43eb43ed9
abhishek1165 <abhishekkachha12345@gmail.com>
2025-01-29 13:17:47 +0530

    solved error on ConvexClerkProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { use } from "react";
 
 
 export default function Home() {
   return (
-   <div className="m-10 text-cyan-300 rounded-md bg-black w-20  px-4">Home </div>
+    <SignedOut>
+   <SignInButton>
+
+    <Button>Sign In</Button>
+   </SignInButton>
+   </SignedOut>
   );
 }
