b101f1c1419af3fb73768bbeb0c3e25d2ab5793d
abhishek1165 <abhishekkachha12345@gmail.com>
2025-03-03 13:34:39 +0530

    video meeting setup complete made streamclientprovider, meetingroom, meetingsetup and hooks for meeting user id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "./globals.css";
 import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import ConvexClerkProvider from "@/components/providers/ConvexClerkProvider";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -51,6 +53,7 @@ export default function RootLayout({
           <RedirectToSignIn />
          </SignedOut>
          </ThemeProvider>
+         <Toaster/>
         </body>
       </html>
     </ConvexClerkProvider>
--- src/components/MeetingModal.tsx
+++ src/components/MeetingModal.tsx
@@ -2,6 +2,7 @@ import { Button } from "./ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 import { useState } from "react";
 import { Input } from "./ui/input";
+import useMeetingActions from "@/hooks/useMeetingActions";
 
 interface MeetingModalProps {
     isOpen: boolean;
@@ -14,11 +15,21 @@ interface MeetingModalProps {
 
 function MeetingModal({isOpen,onClose,title,isJoinMeeting}:MeetingModalProps) {
     const [meetingUrl,setMeetingUrl]=useState("");
+    const {createInstantMeeting,joinMeeting}= useMeetingActions();
+  
 
-    const createMeeting=async()=>{}
-    const joinMeeting=async()=>{}
-
-    const handleStart=()=>{}
+    const handleStart=()=>{
+      if (isJoinMeeting) {
+        // if it's a full URL extract meeting ID
+        const meetingId = meetingUrl.split("/").pop();
+        if (meetingId) joinMeeting(meetingId);
+      } else {
+        createInstantMeeting();
+      }
+  
+      setMeetingUrl("");
+      onClose();
+    }
 
     return (
     <Dialog open ={isOpen} onOpenChange={onClose} >
